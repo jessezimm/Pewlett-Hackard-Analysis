a7770d05056842b307d7deaea73c1f074df01535
jessezimm <jessezimm@gmail.com>
2021-10-23 11:27:40 -0400

    creating tables

--- schema.sql
+++ schema.sql
@@ -34,3 +34,21 @@ CREATE TABLE salaries (
   FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
   PRIMARY KEY (emp_no)
 );
+
+CREATE TABLE dept_emp (
+  dept_no INT NOT NULL,
+  emp_no INT NOT NULL,
+  from_date DATE NOT NULL,
+  to_date DATE NOT NULL,
+  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+  PRIMARY KEY (emp_no)
+);
+
+CREATE TABLE titles (
+  emp_no INT NOT NULL,
+  title INT NOT NULL,
+  from_date DATE NOT NULL,
+  to_date DATE NOT NULL,
+  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+  PRIMARY KEY (emp_no)
+);
